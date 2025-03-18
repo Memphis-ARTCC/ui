23185b40893a0a2e132fb38f3e108234a7e051e4
Jacob Boyles <fixterjake@gmail.com>
2025-03-18 16:06:27 -0400

    upgrade some packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,22 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
     darkMode: ["class"],
     content: [
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}"
     ],
-    plugins: [require("tailwindcss-animate")],
+    plugins: [tailwindcssAnimate],
     theme: {
-    	extend: {
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		},
-    		colors: {}
-    	}
+        extend: {
+            borderRadius: {
+                lg: "var(--radius)",
+                md: "calc(var(--radius) - 2px)",
+                sm: "calc(var(--radius) - 4px)"
+            },
+            colors: {}
+        }
     }
 } satisfies Config;
